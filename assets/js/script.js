67ed56e7999963b6b4a6ebbbc7bcb7c5f45bfc88
Thiago Alves <thiagosobral23@gmail.com>
2024-01-13 12:55:16 +0000

    Add declaration of variables and an empty array of the game box

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,16 @@
 // Wait for the DOM to finish loading before running the game
 document.addEventListener("DOMContentLoaded", function() {
-    
+    // Declaration of the variables
+    let boxes = document.getElementsByClassName('position');
+    let gameStatus = document.getElementById('gameStatus');
+    let restart = document.getElementById('restart');
+    // Create a array of empty value
+    let boxValue = Array(9).fill(null);
+    // // Declare currentPlayer as a global variable to start the game 
+    let currentPlayer = "X";
+
+    console.log(boxValue)
+
 });
 
 function runGame() {
